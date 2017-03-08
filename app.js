3315603deb21e11d6633789feda56065aa7eb9da
YuTong-Zhang <yz60@rice.edu>
2017-03-07 22:13:55 -0600

    Added forgot password and email verification

--- app.js
+++ app.js
@@ -29,6 +29,13 @@
     const email = txtEmail.value;
     const password = txtPassword.value;
     const auth = firebase.auth();
+    // auth.createUserWithEmailAndPassword(email, password).then(user =>{
+    //   const promise = user.sendEmailVerification();
+    //   promise.catch(e => alert(e.message));
+    // }, error =>{
+    //   alert(error.message);
+    // });
+
     const promise = auth.createUserWithEmailAndPassword(email, password);
     promise.catch(e => alert(e.message));
 
@@ -37,7 +44,23 @@
   //btnLogout.addEventListener('click', e =>{
     //firebase.auth().signOut();
   //});
-
+  const forgotPW = document.querySelectorAll("#forgotPW")[0];
+  forgotPW.addEventListener('click', e =>{
+    const email = txtEmail.value;
+    if(!email) {
+        alert('Please enter Email Address and re-click "Forgot password?" to get password recovery email.')
+      }
+      var auth = firebase.auth();
+      auth.sendPasswordResetEmail(email).then(function() {
+         alert('A password reset email has been sent to your email address.')
+      }, function(error) {
+        if(error.code == "auth/invalid-email") {
+          alert('Please enter your Email Address and re-click "Forgot password?" to get password recovery email.');
+        } else {
+          alert(error.message);
+        }
+      });
+  });
   // function forgotPW(emailAddress) {
   //   if(!email) {
   //     alert('Please enter Email Address and re-click "Forgot password?" to get password recovery email.')
@@ -56,21 +79,32 @@
 
 
   firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser){
-      console.log(firebaseUser);
-      var userID =  firebaseUser.uid;
-      var userEmail = firebaseUser.email;
-      users.set({
-        UserID: userID
-      });
-      var usersMap = firebase.database().ref("Users Map/" + userID);
-      usersMap.set({
-        email: firebaseUser.email
-      });
-      localStorage.setItem('userid', userID);
-      window.location = 'PresetPage';
-    }else{
-      console.log("Not Logged In");
+    if(firebaseUser.emailVerified){
+      if(firebaseUser){
+        console.log(firebaseUser);
+        var userID =  firebaseUser.uid;
+        var userEmail = firebaseUser.email;
+        users.set({
+          UserID: userID
+        });
+        var usersMap = firebase.database().ref("Users Map/" + userID);
+        usersMap.set({
+          email: firebaseUser.email
+        });
+        localStorage.setItem('userid', userID);
+        window.location = 'PresetPage';
+      }else{
+        console.log("Not Logged In");
+      }
+    }
+    else{
+      if(firebaseUser){
+        firebaseUser.sendEmailVerification().then(function(){
+          alert("A verification email has been sent to you, please verify prior to logging in.");
+        },function(error){
+          alert(error.message);
+        })
+      }
     }
   });
 
